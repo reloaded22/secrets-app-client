61deeb65cad52e7bd4ada301a8e3041bf3106b03
reloaded22 <andres.rojas.talent@gmail.com>
2022-12-09 11:58:23 -0500

    Implement MVC architecture

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from 'axios';
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
-import Secrets from "./components/Secrets";
-import About from "./components/About";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
-import MySecrets from "./components/MySecrets";
-import Edit from "./components/Edit";
-import Submit from "./components/Submit";
-import Register from "./components/Register";
-import Admin from "./components/Admin";
-import AdminEdit from "./components/AdminEdit";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Footer from "./Footer";
+import Secrets from "./Secrets";
+import About from "./About";
+import Login from "./Login";
+import Profile from "./Profile";
+import MySecrets from "./MySecrets";
+import Edit from "./Edit";
+import Submit from "./Submit";
+import Register from "./Register";
+import Admin from "./Admin";
+import AdminEdit from "./AdminEdit";
 
 
 function App() {
-  console.log('localStorage.getItem("session"):');
-  console.log(JSON.parse(localStorage.getItem("session")));
+  // console.log('localStorage.getItem("session"):');
+  // console.log(JSON.parse(localStorage.getItem("session")));
 
   localStorage.setItem(
     "session",
@@ -30,7 +30,6 @@ function App() {
   const [loggedUser, setLoggedUser] = useState({});
 
   useEffect(() => {
-    console.log("***useEffect hook from home executed***");
     axios
       .get("/api/home")
       .then((res) => {
